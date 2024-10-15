fe46eec7f2217bcea2c45e4c2a45c7d69ac571d8
Tinat1ni <tinatingagnidze121@gmail.com>
2024-10-15 22:54:29 +0400

    updated urls so image is visible

--- store/urls.py
+++ store/urls.py
@@ -1,6 +1,4 @@
 from django.urls import path
-from django.conf import settings
-from django.conf.urls.static import static
 from .views import category_list, category_products, product_details
 
 urlpatterns = [
@@ -10,4 +8,3 @@ urlpatterns = [
 
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
